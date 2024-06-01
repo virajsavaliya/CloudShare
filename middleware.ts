222e8934b8181f8d23e9e6fe0db1c8fe1204cb59
Viraj Savaliya <103499143+virajsavaliya@users.noreply.github.com>
2024-06-01 12:04:43 +0530

    Fix middleware edge function error

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
+export const config = {
+  runtime: 'experimental-edge', // Ensure this is correctly set
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
 
 export default clerkMiddleware();
 
 
 
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};

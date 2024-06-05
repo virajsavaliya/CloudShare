a892450d9a4f85b73f0511ecf59d386db33a33e0
Viraj Savaliya <103499143+virajsavaliya@users.noreply.github.com>
2024-06-05 18:06:12 +0530

    Update layout.js

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,21 +1,38 @@
-import React from 'react'
-import SideNav from './_components/SideNav'
-import TopHeader from './_components/TopHeader'
+"use client"
+import React, { useState, useEffect } from 'react';
 
-function layout({children}) {
+import SideNav from './_components/SideNav';
+import TopHeader from './_components/TopHeader';
+import Footer from '../_components/Footer';
+
+function Layout({ children }) {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768); // Adjust the threshold as needed
+    };
+
+    handleResize(); // Call once on initial render to set the initial state
+    window.addEventListener('resize', handleResize); // Add event listener for window resize
+
+    return () => {
+      window.removeEventListener('resize', handleResize); // Clean up event listener
+    };
+  }, []); // Empty dependency array to run the effect only once on initial render
+
   return (
     <div>
-        <div className=' h-full md:w-64 flex-col fixed inset-y-0 z-50
-        md:flex hidden'>
-          <SideNav/>
-        </div>
-        <div className='md:ml-64'>
-          <TopHeader className="flex-1"/>
+      <div className='h-full md:w-64 flex-col fixed inset-y-0 z-50 md:flex hidden'>
+        <SideNav/>
+      </div>
+      <div className='md:ml-64'>
+        <TopHeader className="flex-1"/>
         {children}
-        </div>
-        
-        </div>
-  )
+      </div>
+      {isSmallScreen && <Footer />} {/* Render footer only on small screens */}
+    </div>
+  );
 }
 
-export default layout
+export default Layout;

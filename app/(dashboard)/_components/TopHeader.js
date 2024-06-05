affca5c70578c8f6222484802840e7d66f3420f9
Viraj Savaliya <103499143+virajsavaliya@users.noreply.github.com>
2024-06-05 17:47:44 +0530

    footer and header changes

--- app/(dashboard)/_components/TopHeader.js
+++ app/(dashboard)/_components/TopHeader.js
@@ -10,7 +10,7 @@ function TopHeader() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
   };
 
   const closeMenu = () => {
@@ -22,46 +22,40 @@ function TopHeader() {
       <div className="relative md:hidden">
         <div className="flex px-4 py-1 border-b items-center justify-between">
           <button onClick={toggleMenu}>
-            <AlignJustify />
+            <AlignJustify size={24} /> {/* Increased icon size */}
           </button>
           <Image src="/logo.png" width={150} height={100} alt="Logo" />
           <UserButton />
         </div>
 
-        {menuOpen && (
-          <div className="absolute top-full left-0 w-full bg-white shadow-md">
-            <nav className="flex flex-col p-5">
-              <Link
-                href="/"
-                onClick={closeMenu}
-                className="py-2 border-b flex items-center"
-              >
-                <MdHome className="mr-2" /> Home
-              </Link>
-              <Link
-                href="/files"
-                onClick={closeMenu}
-                className="py-2 border-b flex items-center"
-              >
-                <MdFolder className="mr-2" /> Files
-              </Link>
-              <Link
-                href="/upload"
-                onClick={closeMenu}
-                className="py-2 border-b flex items-center"
-              >
-                <MdCloudUpload className="mr-2" /> Upload
-              </Link>
-              <Link
-                href="/upgrade"
-                onClick={closeMenu}
-                className="py-2 border-b flex items-center"
-              >
-                <MdShield className="mr-2" /> Upgrade
-              </Link>
-            </nav>
-          </div>
-        )}
+        <div className={`menu-container ${menuOpen ? 'open' : ''}`}>
+          <nav className="menu">
+            <Link href="/" onClick={closeMenu} className="menu-item">
+              <div className="flex items-center">
+                <MdHome className="mr-2 text-lg" /> {/* Increased icon size */}
+                <span className="text-lg">Home</span> {/* Increased text size */}
+              </div>
+            </Link>
+            <Link href="/files" onClick={closeMenu} className="menu-item">
+              <div className="flex items-center">
+                <MdFolder className="mr-2 text-lg" /> {/* Increased icon size */}
+                <span className="text-lg">Files</span> {/* Increased text size */}
+              </div>
+            </Link>
+            <Link href="/upload" onClick={closeMenu} className="menu-item">
+              <div className="flex items-center">
+                <MdCloudUpload className="mr-2 text-lg" /> {/* Increased icon size */}
+                <span className="text-lg">Upload</span> {/* Increased text size */}
+              </div>
+            </Link>
+            <Link href="/upgrade" onClick={closeMenu} className="menu-item">
+              <div className="flex items-center">
+                <MdShield className="mr-2 text-lg" /> {/* Increased icon size */}
+                <span className="text-lg">Upgrade</span> {/* Increased text size */}
+              </div>
+            </Link>
+          </nav>
+        </div>
       </div>
     </div>
   );

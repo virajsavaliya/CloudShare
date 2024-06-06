2ee42052270894136d468dfefcbc494490f305a3
Viraj Savaliya <103499143+virajsavaliya@users.noreply.github.com>
2024-06-06 16:44:27 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
-import Footer from "./_components/CookieConsent";
 
 export default function Home() {
   return (
     <div>
-    <CookieConsent/>
     <Header/>
     <Hero/>
     <Footer/>

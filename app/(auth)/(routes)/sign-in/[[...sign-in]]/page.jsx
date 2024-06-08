239c9de1e1288791f4f57f06470efaaa6eac2a79
Viraj Savaliya <103499143+virajsavaliya@users.noreply.github.com>
2024-06-08 10:37:03 +0530

    Update page.jsx

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
 
 
-<section className="bg-white">
+<section className="bg-gray-100">
   <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
     <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
     <Image
